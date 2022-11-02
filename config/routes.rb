8fc848f629708fe0c67508d59b26cd1536415625
tonysnickers <antoine.mendy@hotmail.fr>
2022-11-02 16:36:17 +0100

    style added to argonautes lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :argonautes
+  resources :argonautes, only: [:index, :new, :create]
 end
