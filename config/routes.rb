d9ba27d986b73d2fe656b88261e13e6c20bc610f
tonysnickers <antoine.mendy@hotmail.fr>
2022-11-03 01:24:12 +0100

    index route is now the principal route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :argonautes, only: [:index, :new, :create]
+  root to: 'argonautes#index'
+  resources :argonautes, only: [:new, :create]
 end
